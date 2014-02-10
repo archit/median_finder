d6caf5459c3eb6113654d9bf72096d251c9a1a57
Archit Baweja <architbaweja@gmail.com>
2014-02-10 00:57:18 -0800

    Comments

--- tests/median_finder_tests.py
+++ tests/median_finder_tests.py
@@ -11,8 +11,11 @@ def file_len(fname):
 
 # Test working with files
 def test_files():
+    # Run through a couple file test cases...
     for filename in ["input1", "input2"]:
         filename_with_path = os.path.abspath("tests/{}.txt".format(filename))
+
+        # ... with all types of bucket sizes.
         for bucket_size in range(1, file_len(filename_with_path)):
             with open(filename_with_path) as file:
                 actual   = find_median(file, bucket_size)
